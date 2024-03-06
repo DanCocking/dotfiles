fdd0c5e12aa175b66e12822bba483f8616bebe77
Dan Cocking <cockingdan@gmail.com>
2024-03-06 17:39:28 +1100

    Add fugitive

--- .config/nvim/lua/dan/lazy_init.lua
+++ .config/nvim/lua/dan/lazy_init.lua
@@ -5,8 +5,8 @@ if not vim.loop.fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
+require('lazy').setup {
+  spec = 'dan.plugins',
+  change_detection = { notify = false },
+}
 
-require("lazy").setup({
-    spec = "dan.plugins",
-    change_detection = { notify = false }
-})
